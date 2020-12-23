3317aeaaa944a957b539e01e2b4d2081cadb3a53
organtin <giovanni.organtini@roma1.infn.it>
2020-12-23 11:15:09 +0100

    code cleanup

--- mtdConstructionDBTools/mtdcdb.py
+++ mtdConstructionDBTools/mtdcdb.py
@@ -1,6 +1,6 @@
 from lxml import etree
-import random
 import sys
+import getpass
 
 def attribute(parent, name, value):
     attribute = etree.SubElement(parent, "ATTRIBUTE")
@@ -22,3 +22,51 @@ def part(barcode, kind_of_part, attributes, user = 'organtin', location = 'testL
 def root():
     root = etree.Element("ROOT", encoding = 'xmlns:xsi=http://www.w3.org/2001/XMLSchema-instance')
     return root
+
+def mtdcreateMatrix(barcode, Xtaltype, producer, batchIngot, laboratory):
+    # create root element
+    root = etree.Element("ROOT", encoding = 'xmlns:xsi=http://www.w3.org/2001/XMLSchema-instance')
+    parts = etree.SubElement(root, "PARTS")
+
+    # build the list of the attributes
+    attrs = []
+    attr = {}
+    attr['NAME'] = 'PRODUCER'
+    attr['VALUE'] = producer
+    attrs.append(attr)
+
+    # create the batch/ingot pair (the father)
+    bi = part(str(batchIngot), 'Batch/Ingot', attrs, user = getpass.getuser(), location = laboratory)
+    attrs = []
+
+    # create the matrix part (a child of the batch)
+    matrix = etree.SubElement(bi, "CHILDREN")
+    LYSOMatrixtype = f'LYSOMatrix #{Xtaltype}'
+    matrixxml = part(barcode, LYSOMatrixtype, attrs, user = getpass.getuser(), location = laboratory)
+
+    # append the child to the father
+    matrix.append(matrixxml)
+
+    # create the single crystals as children of the matrix
+    for i in range(16):
+        singlextal = etree.SubElement(matrix, "CHILDREN")
+        cbarcode = barcode + '-' + str(i)
+        xtal = part(cbarcode, 'singleBarCrystal', [], user = getpass.getuser(), location = laboratory)
+        singlextal.append(xtal)
+
+    # append the father to the root node
+    parts.append(bi)
+        
+    # parint xml
+    xml = etree.tostring(root, encoding='UTF-8', standalone = 'yes', xml_declaration=True, pretty_print=True)
+    print(xml.decode("utf-8"))
+
+def mtdhelp(shrtOpts, longOpts, helpOpts, err = 0, hlp = ''):
+    print('Usage: registerMatrix.py [options]')
+    print('       ' + hlp)
+    print()
+    shrtOpts = shrtOpts.replace(':', '')
+    longOpts = [s.replace('=', '=<value>') for s in longOpts]
+    for i in range(len(shrtOpts)):
+        print('       ' + shrtOpts[i] + ' ('+ longOpts[i] + '): ' + helpOpts[i])
+    exit(err)
--- registerMatrix.py
+++ registerMatrix.py
@@ -7,15 +7,13 @@ This script is used to generate the XML code needed to register an MTD LYSO matr
 MTD database. For information about how to use it, just run it.
 
 '''
-from lxml import etree
-import random
-import getpass
 import sys
 import getopt
 from mtdConstructionDBTools import mtdcdb
 
 PRODUCER_MAX = 10
 
+# define command line options
 shrtOpts = 'hb:x:p:t:l:'
 longOpts = ['help', 'batch=', 'xtal=', 'producer=', 'type=', 'lab=']
 helpOpts = ['shows this help', 'specify the batch to which the matrix belongs',
@@ -23,20 +21,13 @@ helpOpts = ['shows this help', 'specify the batch to which the matrix belongs',
             'specify the producer index [0 < index < {}]'.format(PRODUCER_MAX),
             'specify the matrix type (thickness) [1, 2 or 3]',
             'specify the laboratory in which the registration is done (default = Roma)']
+hlp = 'Generates the XML needed to register a LYSO matrix into the MTD construction DB. Options follows.' \
+    '\n       Example python3 {} --batch test-2020 --x 3310100000013 --producer 4 --t 1'.format(sys.argv[0])
 
-def mtdhelp(shrtOpts, longOpts, helpOpts, err = 0):
-    print('Usage: registerMatrix.py [options]')
-    print('       options')
-    shrtOpts = shrtOpts.replace(':', '')
-    longOpts = [s.replace('=', '=<value>') for s in longOpts]
-    for i in range(len(shrtOpts)):
-        print(shrtOpts[i] + ' ('+ longOpts[i] + '): ' + helpOpts[i])
-    exit(err)
-    
 try:
     opts, args = getopt.getopt(sys.argv[1:], shrtOpts, longOpts)
 except:
-    mtdhelp(shrtOpts, longOpts, helpOpts, -1)
+    mtdcdb.mtdhelp(shrtOpts, longOpts, helpOpts, -1, hlp)
 
 batchIngot = ''
 barcode = ''
@@ -46,7 +37,7 @@ laboratory = 'Roma'
 
 for o, a in opts:
     if o in ('h', '--help'):
-        mtdhelp(shrtOpts, longOpts, helpOpts)
+        mtdcdb.mtdhelp(shrtOpts, longOpts, helpOpts, hlp)
     elif o in ('b', '--batch'):
         batchIngot = a
     elif o in ('x', '--xtal'):
@@ -66,44 +57,49 @@ for o, a in opts:
         assert False, 'unhandled option'
 
 if batchIngot == '' or barcode == '' or producer == '':
-    mtdhelp(shrtOpts, longOpts, helpOpts, -2)
+    mtdcdb.mtdhelp(shrtOpts, longOpts, helpOpts, -2, hlp)
 
-# create root element
-root = mtdcdb.root()
-parts = etree.SubElement(root, "PARTS")
+'''
+def mtdcreateMatrix(barcode, producer, batchIngot, laboratory):
+    # create root element
+    root = mtdcdb.root()
+    parts = etree.SubElement(root, "PARTS")
 
-# build the list of the attributes
-attrs = []
-attr = {}
-attr['NAME'] = 'PRODUCER'
-attr['VALUE'] = producer
-attrs.append(attr)
+    # build the list of the attributes
+    attrs = []
+    attr = {}
+    attr['NAME'] = 'PRODUCER'
+    attr['VALUE'] = producer
+    attrs.append(attr)
 
-# create the batch/ingot pair (the father)
-bi = mtdcdb.part(str(batchIngot), 'Batch/Ingot', attrs, user = getpass.getuser(), location = laboratory)
-attrs = []
+    # create the batch/ingot pair (the father)
+    bi = mtdcdb.part(str(batchIngot), 'Batch/Ingot', attrs, user = getpass.getuser(), location = laboratory)
+    attrs = []
 
-# create the matrix part (a child of the batch)
-matrix = etree.SubElement(bi, "CHILDREN")
-LYSOMatrixtype = f'LYSOMatrix #{Xtaltype}'
-matrixxml = mtdcdb.part(barcode, LYSOMatrixtype, attrs, user = getpass.getuser(), location = laboratory)
+    # create the matrix part (a child of the batch)
+    matrix = etree.SubElement(bi, "CHILDREN")
+    LYSOMatrixtype = f'LYSOMatrix #{Xtaltype}'
+    matrixxml = mtdcdb.part(barcode, LYSOMatrixtype, attrs, user = getpass.getuser(), location = laboratory)
 
-# append the child to the father
-matrix.append(matrixxml)
+    # append the child to the father
+    matrix.append(matrixxml)
 
-# create the single crystals as children of the matrix
-for i in range(16):
-    singlextal = etree.SubElement(matrix, "CHILDREN")
-    cbarcode = barcode + '-' + str(i)
-    xtal = mtdcdb.part(cbarcode, 'singleBarCrystal', [], user = getpass.getuser(), location = laboratory)
-    singlextal.append(xtal)
+    # create the single crystals as children of the matrix
+    for i in range(16):
+        singlextal = etree.SubElement(matrix, "CHILDREN")
+        cbarcode = barcode + '-' + str(i)
+        xtal = mtdcdb.part(cbarcode, 'singleBarCrystal', [], user = getpass.getuser(), location = laboratory)
+        singlextal.append(xtal)
 
-# append the father to the root node
-parts.append(bi)
+    # append the father to the root node
+    parts.append(bi)
+        
+    # parint xml
+    xml = etree.tostring(root, encoding='UTF-8', standalone = 'yes', xml_declaration=True, pretty_print=True)
+    print(xml.decode("utf-8"))
+'''
 
-# parint xml
-xml = etree.tostring(root, encoding='UTF-8', standalone = 'yes', xml_declaration=True, pretty_print=True)
-print(xml.decode("utf-8"))
+mtdcdb.mtdcreateMatrix(barcode, Xtaltype, producer, batchIngot, laboratory)
 
 exit(0)
 
